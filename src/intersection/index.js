2a4e170c0978033639b8345d50892b5815e62414
jbrackin1 <jbrackin1@gmail.com>
2022-12-26 18:48:56 -0600

    all working

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,16 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+    let map = new Map();
+    let results = new Set();
+
+    for (let e of a){
+        map.set(e,e)
+    }
+    for (let e of b){
+        if (map.has(e)){
+            results.add(e)
+        }
+    }
+    return Array.from(results);
+}
 
 module.exports = intersection;
--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,23 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+    let numbers = new Map();
+    let solution = new Map();
+
+    for (let e of A){
+        numbers.set(e, e)
+    }
+    for (let e of A){
+        let diff = N - e;
+        if (diff !== e){
+            if (numbers.has(diff)){
+                solution.set(Math.min(e, diff), Math.max(e, diff))
+            }
+        }
+    }
+
+    return Array.from(solution);
+}
 
 module.exports = sumPairs;
